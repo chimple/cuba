91bac87f367edda8f0d161e9eac384cb2897cd35
Mary-Harshitha <62737530+Mary-Harshitha@users.noreply.github.com>
2023-02-07 09:52:30 +0530

    pattern as image texture

--- src/components/LessonCard.tsx
+++ src/components/LessonCard.tsx
@@ -5,6 +5,7 @@ import { LESSON_CARD_COLORS, PAGES } from "../common/constants";
 import { Lesson } from "../interface/curriculumInterfaces";
 import "./LessonCard.css";
 import ScoreCard from "./ScoreCard";
+import styled from 'styled-components';
 
 const LessonCard: React.FC<{
   width: string;
@@ -12,6 +13,7 @@ const LessonCard: React.FC<{
   lesson: Lesson;
   isPlayed: boolean;
   isUnlocked: boolean;
+  showSubjectName: boolean;
   showText?: boolean;
   showScoreCard?: boolean;
   score: any;
@@ -21,6 +23,7 @@ const LessonCard: React.FC<{
   lesson,
   isPlayed,
   isUnlocked,
+  showSubjectName = false,
   showText = true,
   showScoreCard = true,
   score,
@@ -35,8 +38,6 @@ const LessonCard: React.FC<{
   const lessonCardColor = lesson?.color
     ? lesson.color
     : LESSON_CARD_COLORS[Math.floor(Math.random() * LESSON_CARD_COLORS.length)];
-
-  const lessonCard = LESSON_CARD_IMAGES[Math.floor(Math.random() * LESSON_CARD_IMAGES.length)];
 
   return (
     <IonCard
@@ -76,37 +77,55 @@ const LessonCard: React.FC<{
       }}
       // disabled={!isUnlocked}
     >
-      <div
+      <div 
         style={{
           display: "grid",
         }}
       >
-        
-          
-        
         <div
-        >
-          <div >
+        style={{
+          background: lessonCardColor,
+          borderRadius: "30px",
+          width: width,
+          height: height,
+          display: "grid",
+          justifyContent: "center",
+          alignItems: "center",
+          gridArea: "1/1",
+          filter: isUnlocked ? 'none' :'grayscale(90%)',
+        }}
+        color={lesson?.color}>
+          {showSubjectName ? (
+            <div id="lesson-card-subject-name">
+              <p>{lesson?.chapter.course.name}</p>
+            </div>
+          ) : null}
+            <Container><StyledSVG color={lessonCardColor} width={width} height={height}>
+              <PatternSVGImg></PatternSVGImg>
+              </StyledSVG></Container>
+              
+              <div id="lesson-card-image">
             {showImage ? (
               <img
+                id="lesson-card-image"
                 loading="lazy"
-                alt={"assets/icons/Card_A.svg"}
-                src={lessonCard}
+                alt={"courses/" + subjectCode + "/icons/" + lesson.image}
+                src={"courses/" + subjectCode + "/icons/" + lesson.image}
                 onError={hideImg}
               />
             ) : (
               <div /> // we can show Default LessonCard text or image
             )}
+             
+            
             {!isUnlocked ? (
               <div id = "lesson-card-status-icon">
               <img
-                id="lesson-card-status-icon1"
+              id="lesson-card-status-icon1"
                 loading="lazy"
-                src="assets/icons/LockIcon.svg"
-                alt="LockIcon"
-              />
-               </div>
-              
+                src="assets/icons/Lock_icon.svg"
+                alt="assets/icons/Lock_icon.svg"
+              /></div>
             ) : isPlayed ? (
               showScoreCard ? (
                 <div id="lesson-card-score">
@@ -118,9 +137,9 @@ const LessonCard: React.FC<{
             ) : (
               <div />
             )}
+            
           </div>
-          
-        </div>
+          </div>
       </div>
       {showText ? (
         <p
@@ -137,11 +156,44 @@ const LessonCard: React.FC<{
 };
 
 export default LessonCard;
+const PatternSVGImg =() => {
+  return(
+    <svg id="Layer_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 508.9 566.7">
+        <g id="Graphic_Elements">
+          <rect  width="508.9" height="566.7" rx="64.84" ry="64.84"/>
+          <g opacity ='.15'>
+            <path d="m444.93,0H64.08C28.69,0,0,28.69,0,64.08v21.89c38.27,2.99,66-9.52,96.92-39.45,50.95-49.33,90.18-10.19,163.44,51.97,77.45,65.72,192.68-64.96,248.54-34.51h0C508.9,28.64,480.26,0,444.93,0Z"/>
+            <circle  cx="217.38" cy="26.62" r="13.87"/>
+            <circle  cx="325.28" cy="37.47" r="10.85"/>
+            <circle  cx="268.1" cy="83.88" r="7.19"/>
+            <circle  cx="396.6" cy="71.8" r="7.19"/>
+            <circle  cx="485.31" cy="44.93" r="4.25"/>
+            <circle  cx="13.36" cy="71.8" r="4.25"/>
+            <circle  cx="66.55" cy="57.81" r="3"/>
+            <circle  cx="42.64" cy="17.44" r="3"/>
+            <circle  cx="92.83" cy="14.43" r="3"/>
+            <circle  cx="429.95" cy="15.78" r="4.25"/></g></g></svg>
+  );
+};
+const Container = styled.div`
+  background-size: cover;
+  background-repeat: no-repeat;
+  border-radius: 30px;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  grid-area: 1/1;
+  opacity: "0.3",
+`;
 
-const LESSON_CARD_IMAGES = [
-  "assets/icons/Card_A_1.png",
-  "assets/icons/Card_B_1.png",
-  "assets/icons/Card_C_1.png"
-]
-//.svg
-//_1.png
+
+const StyledSVG = styled.svg`
+& rect {
+  fill: ${props => props.color};
+}
+& circle {
+  fill: ${props => props.color};
+}
+`;
+
+
