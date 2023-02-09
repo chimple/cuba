e0c399f5f36154bcb2ed049d07e77d676d32bd9a
Bhanu <62737055+bhanugummana@users.noreply.github.com>
2023-02-09 08:26:53 +0530

    Fixed i18n local loadPath

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,21 +1,23 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import Backend from 'i18next-http-backend';
+import Backend from "i18next-http-backend";
 import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
+import { BASE_NAME } from "./common/constants";
 
 i18n
   .use(Backend)
   .use(I18nextBrowserLanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    fallbackLng: 'en',
+    fallbackLng: "en",
     debug: true,
-    // lng: "hi",
-
+    backend: {
+      loadPath: BASE_NAME + "/locales/{{lng}}/{{ns}}.json",
+    },
 
     interpolation: {
-      escapeValue: false // react already safes from xss
-    }
+      escapeValue: false, // react already safes from xss
+    },
   });
 
 export default i18n;
