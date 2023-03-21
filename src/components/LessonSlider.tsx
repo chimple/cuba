4e1e650b3ac7822b481b51f08e8fb14a85eaad7d
Mary-Harshitha <62737530+Mary-Harshitha@users.noreply.github.com>
2023-03-21 09:48:46 +0530

    lessonslider to show arrows

--- src/components/LessonSlider.tsx
+++ src/components/LessonSlider.tsx
@@ -1,11 +1,17 @@
 import "./LessonSlider.css";
+import "./LessonCard.css";
 import LessonCard from "./LessonCard";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import { Lesson } from "../interface/curriculumInterfaces";
 import { useEffect, useState } from "react";
+import Arrow from "./arrow";
+import { Chapter } from "../interface/curriculumInterfaces";
 
 const LessonSlider: React.FC<{
   lessonData: Lesson[];
+  currentChapter: Chapter;
+  onChapterChange;
+  isHome: boolean;
   onSwiper: any;
   // onSlideChange: Function;
   lessonsScoreMap: any;
@@ -13,13 +19,20 @@ const LessonSlider: React.FC<{
   showSubjectName: boolean;
 }> = ({
   lessonData,
+  currentChapter,
+  isHome,
   onSwiper,
+  onChapterChange,
   // onSlideChange,
   lessonsScoreMap,
   startIndex,
   showSubjectName = false,
 }) => {
   const [lessonSwiperRef, setLessonSwiperRef] = useState<any>();
+  let width1: string;
+  let height1: string;
+  let playCard: boolean;
+  let checkSecond: boolean;
   useEffect(() => {
     console.log(
       "🚀 ~ file: LessonSlider.tsx:24 ~ useEffect ~ useEffect:startIndex",
@@ -28,9 +41,10 @@ const LessonSlider: React.FC<{
     lessonSwiperRef?.go(0);
     setTimeout(() => {
       if (startIndex) lessonSwiperRef?.go(startIndex);
-    }, 100);
+      console.log('timeout',lessonSwiperRef)
+    }, 100); 
   });
-  return (
+  return(isHome?(
     <div className="content">
       <Splide
         ref={setLessonSwiperRef}
@@ -47,24 +61,156 @@ const LessonSlider: React.FC<{
           if (!m) return;
           const isPlayed =
             !!lessonsScoreMap[m.id] && lessonsScoreMap[m.id]?.score > 0;
+            width1="35vh"
+            height1="40vh"
+            playCard = false
           return (
             <SplideSlide className="slide" key={i}>
               <LessonCard
-                width="40vh"
-                height="45vh"
+                width= {width1}
+                height={height1}
                 isPlayed={isPlayed}
                 isUnlocked={true}
                 lesson={m}
                 showSubjectName={showSubjectName}
                 showScoreCard={isPlayed}
                 score={lessonsScoreMap[m.id]?.score}
-              />
+                toBePlayed={playCard}              />
             </SplideSlide>
           );
         })}
       </Splide>
     </div>
-  );
+  ):(
+    <div className="content">
+      <Splide
+        ref={setLessonSwiperRef}
+        hasTrack={true}
+        options={{
+          arrows: false,
+          wheel: true,
+          lazyLoad: true,
+          direction: "ltr",
+          pagination: false,
+        }}
+      >
+        {lessonData.map((m: Lesson, i: number) => {
+          if (!m) return;
+          const isPlayed =
+            !!lessonsScoreMap[m.id] && lessonsScoreMap[m.id]?.score > 0;
+          width1="35vh"
+          height1="40vh"
+          playCard = false
+          if(i == startIndex){
+            if(isPlayed){
+              width1="35vh"
+              height1="40vh"
+              playCard = false
+              checkSecond = true
+            }
+            else if(!isPlayed){
+              width1 = "40vh"
+              height1 = "50vh"
+              playCard = true
+              checkSecond = false
+            }
+          }
+          if(i === (startIndex + 1) && checkSecond){  
+            width1 = "40vh"
+            height1 = "50vh"
+            playCard = true
+          } 
+          else if(i === (startIndex + 1) && !checkSecond){
+            width1="35vh"
+            height1="40vh"
+            playCard = false
+          }
+          else if(checkSecond) {
+            width1="35vh"
+            height1="40vh"
+            playCard = false
+          }
+          if(i == lessonData.length - 1){
+            console.log("LAST", m)
+          }
+          if(i == lessonData.length-1){
+            return(
+              <><SplideSlide className="slide" key={i}>
+                <LessonCard
+                  width={width1}
+                  height={height1}
+                  isPlayed={isPlayed}
+                  isUnlocked={true}
+                  lesson={m}
+                  showSubjectName={showSubjectName}
+                  showScoreCard={isPlayed}
+                  score={lessonsScoreMap[m.id]?.score}
+                  toBePlayed={playCard} />
+
+              </SplideSlide><SplideSlide className="slide" key={i}>
+                  <Arrow
+                    width={width1}
+                    height={height1}
+                    toBePlayed={playCard}
+                    isForward={true}
+                    imgUrl="assets/icons/forward-arrow.png"
+                    currentChapter={currentChapter!}
+                    onChapterChange={onChapterChange}
+                  ></Arrow>
+                </SplideSlide></>
+            )
+          }
+          else if(i == 0){
+            return(
+              <>
+              <SplideSlide className="slide" key={i}>
+              <Arrow
+                    width={width1}
+                    height={height1}
+                    toBePlayed={playCard}
+                    isForward={false}
+                    imgUrl="assets/icons/previous.png"
+                    currentChapter={currentChapter!}
+                    onChapterChange={onChapterChange}
+                  ></Arrow>
+                </SplideSlide>
+              <SplideSlide className="slide" key={i}>
+                <LessonCard
+                  width={width1}
+                  height={height1}
+                  isPlayed={isPlayed}
+                  isUnlocked={true}
+                  lesson={m}
+                  showSubjectName={showSubjectName}
+                  showScoreCard={isPlayed}
+                  score={lessonsScoreMap[m.id]?.score}
+                  toBePlayed={playCard} />
+
+              </SplideSlide>
+              </>
+            )
+          }
+          else{
+            return (
+              <SplideSlide className="slide" key={i}>
+                <LessonCard
+                  width= {width1}
+                  height={height1}
+                  isPlayed={isPlayed}
+                  isUnlocked={true}
+                  lesson={m}
+                  showSubjectName={showSubjectName}
+                  showScoreCard={isPlayed}
+                  score={lessonsScoreMap[m.id]?.score}
+                  toBePlayed={playCard}
+                />
+              </SplideSlide>
+            )
+          }
+        })}
+      </Splide>
+    </div>
+  ));
 };
 
 export default LessonSlider;
