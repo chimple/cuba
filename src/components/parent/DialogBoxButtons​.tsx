3d86bc309d1028e55967b9dc45056b22babc6d13
Skanda <skanda@sutara.org>
2023-05-04 14:52:39 +0530

    Changed DialogBox UI Component

--- src/components/parent/DialogBoxButtons​.tsx
+++ src/components/parent/DialogBoxButtons​.tsx
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
-import {
-  IonButton,
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
-  IonAlert,
-} from "@ionic/react";
+import React, { MouseEventHandler } from "react";
+import { IonAlert, IonButton, IonIcon } from "@ionic/react";
 import "./DialogBoxButtons.css";
 import { OverlayEventDetail } from "@ionic/react/dist/types/components/react-component-lib/interfaces";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+} from "@mui/material";
+import { t } from "i18next";
+import { chevronForward } from "ionicons/icons";
+import { title } from "process";
+import { IoCloseCircle } from "react-icons/io5";
+import { PAGES } from "../../common/constants";
 
 const DialogBoxButtons: React.FC<{
   width: string;
@@ -18,7 +22,9 @@ const DialogBoxButtons: React.FC<{
   message: string;
   yesText: string;
   noText: string;
-  onButtonClicked: (event: CustomEvent<OverlayEventDetail<any>>) => void;
+  handleClose: (event: CustomEvent<OverlayEventDetail<any>>) => void;
+  onYesButtonClicked: MouseEventHandler<HTMLDivElement>;
+  onNoButtonClicked: MouseEventHandler<HTMLDivElement>;
 }> = ({
   width,
   height,
@@ -26,26 +32,59 @@ const DialogBoxButtons: React.FC<{
   message,
   yesText,
   noText,
-  onButtonClicked,
+  handleClose,
+  onYesButtonClicked,
+  onNoButtonClicked,
 }) => {
   return (
-    <>
-      <IonAlert
-        header={message}
-        isOpen={showDialogBox}
-        onDidDismiss={onButtonClicked}
-        buttons={[
-          {
-            text: yesText,
-            role: "delete",
-          },
-          {
-            text: noText,
-            role: noText,
-          },
-        ]}
-      ></IonAlert>
-    </>
+    <div>
+      <Dialog
+        open={showDialogBox}
+        onClose={handleClose}
+        // style={{ borderRadius: "6vh" }}
+      >
+        <DialogContent
+          style={{
+            // padding: "8% 5%",
+            textAlign: "center",
+            width: width,
+            height: height,
+          }}
+        >
+          <DialogContentText
+            style={{
+              textAlign: "center",
+              color: "black",
+              fontWeight: "normal",
+              margin: "6% 0% 0% 0%",
+            }}
+          >
+            {message}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions
+          style={{
+            justifyContent: "space-around",
+            margin: "0% 0% 5% 0%",
+          }}
+        >
+          <div
+            id={"yes-button"}
+            className="dialog-box-button-style"
+            onClick={onYesButtonClicked}
+          >
+            {yesText}
+          </div>
+          <div
+            id={"no-button"}
+            className="dialog-box-button-style"
+            onClick={onNoButtonClicked}
+          >
+            {noText}
+          </div>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 };
 
