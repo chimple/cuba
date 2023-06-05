c881978d6727d6c2ec304cc2ad98701597241739
Mujafar <ali@sutara.org>
2023-06-05 10:50:18 +0530

    Modified CSS styles in Dialog-box Button

--- src/components/parent/ProfileCard.tsx
+++ src/components/parent/ProfileCard.tsx
@@ -10,6 +10,7 @@ import { AVATARS, PAGES } from "../../common/constants";
 import { Util } from "../../utility/util";
 import DialogBoxButtons from "./DialogBoxButtons​";
 import { ServiceConfig } from "../../services/ServiceConfig";
+import { t } from "i18next";
 
 const ProfileCard: React.FC<{
   width: string;
@@ -127,7 +128,7 @@ const ProfileCard: React.FC<{
         <DialogBoxButtons
           width={"40vw"}
           height={"30vh"}
-          message="Do you want to delete the user?"
+          message={t("Do you want to delete the user?")}
           showDialogBox={showDialogBox}
           yesText="Yes"
           noText="No"
