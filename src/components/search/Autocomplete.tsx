9d1228ceda72a9f554b843aa4dc5835f71d0cd75
Rahul Lanka <122143777+rahul-lanka@users.noreply.github.com>
2023-06-12 12:29:07 +0530

    Keypad pop up working fine now on search page

--- src/components/search/Autocomplete.tsx
+++ src/components/search/Autocomplete.tsx
@@ -3,7 +3,8 @@ import { createElement, Fragment, useEffect, useRef } from "react";
 import { createRoot } from "react-dom/client";
 
 export function Autocomplete(props) {
-  const containerRef = useRef(null);
+  // const containerRef = useRef(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
   const panelRootRef = useRef<any>(null);
   const rootRef = useRef<any>(null);
 
@@ -28,10 +29,40 @@ export function Autocomplete(props) {
       ...props,
     });
 
+    const inputElement = containerRef.current.querySelector("input");
+if (!inputElement || inputElement.value === "") {
+  inputElement?.focus();
+}
+
     return () => {
       search.destroy();
     };
   }, [props]);
 
-  return <div className="auto-complete" ref={containerRef} />;
+  return(
+    <div className="auto-complete" ref={containerRef}>
+     {/* <style>{`
+        .auto-complete {
+          border-radius: 20px;
+          overflow: hidden;
+          border: 1px solid black;
+          padding: none;
+          width: 35%;
+        }
+
+        .auto-complete.focused {
+          box-shadow: 0 0 0 3px blue;
+        }
+
+        .auto-complete input {
+          width: 100%;
+          height:100%;
+          border: none;
+          outline: none;
+          padding: 8px;
+          box-sizing: border-box;
+        }
+      `}</style> */}
+    </div>
+  );
 }
