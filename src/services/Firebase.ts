25eb85c2a36b3ad61f97510c8733932aa25812ae
Mary-Harshitha <62737530+Mary-Harshitha@users.noreply.github.com>
2023-07-06 20:58:43 +0530

    analytics initalization

--- src/services/Firebase.ts
+++ src/services/Firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import {FirebaseAnalytics} from "@capacitor-community/firebase-analytics";
 import {
   CACHE_SIZE_UNLIMITED,
   initializeFirestore,
@@ -15,21 +16,22 @@ import {
 // declare global {
 //   // eslint-disable-next-line no-var
 //   var FIREBASE_APPCHECK_DEBUG_TOKEN: boolean | string | undefined;
-// }
+// }\
 
 export const initializeFireBase = () => {
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: process.env.REACT_APP_API_KEY!,
+        authDomain: process.env.REACT_APP_AUTH_DOMAIN!,
+        projectId: process.env.REACT_APP_PROJECT_ID!,
+        storageBucket: process.env.REACT_APP_STORAGE_BUCKET!,
+        messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID!,
+        appId: process.env.REACT_APP_APP_ID!,
+        measurementId: process.env.REACT_APP_MEASUREMENT_ID!
   };
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig); 
   // Initialize Cloud Firestore and get a reference to the service
 
   // const db = getFirestore(app)
@@ -37,4 +39,6 @@ export const initializeFireBase = () => {
   initializeFirestore(app, {
     localCache: persistentLocalCache({ cacheSizeBytes: CACHE_SIZE_UNLIMITED }),
   });
+
+  FirebaseAnalytics.initializeFirebase(firebaseConfig);
 };
