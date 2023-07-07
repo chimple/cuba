aa1f11b2474a5a95bbd3b9b83839efc388a92d1b
Mujafar <ali@sutara.org>
2023-07-08 00:39:07 +0530

    Added A New Score Card

--- src/pages/CocosGame.tsx
+++ src/pages/CocosGame.tsx
@@ -16,7 +16,9 @@ import {
   CocosLessonData,
 } from "../common/courseConstants";
 import { ServiceConfig } from "../services/ServiceConfig";
-import DialogBoxButtons from "../components/parent/DialogBoxButtons​";
+import NewScoreCard from "../components/parent/NewScoreCard";
+import ScoreCard from "../components/ScoreCard";
+import { AiTwotoneHeart } from "react-icons/ai";
 
 const CocosGame: React.FC = () => {
   const history = useHistory();
@@ -27,8 +29,8 @@ const CocosGame: React.FC = () => {
   const [isLoading, setIsLoading] = useState<any>();
   const [present] = useIonToast();
   const [showDialogBox, setShowDialogBox] = useState(false);
-  // let gameResult : any; 
-  const [gameResult, setGameResult]=useState<any>();
+  // let gameResult : any;
+  const [gameResult, setGameResult] = useState<any>();
 
   const presentToast = async () => {
     await present({
@@ -90,23 +92,30 @@ const CocosGame: React.FC = () => {
     //   push();
     // };
 
-    document.body.addEventListener(LESSON_END, (event)=>{
-      // setGameResult(event.detail as lessonEndData);
-      setGameResult(event);
-      console.log("----------line 91 add event listener------", event);
-    }, { once: true });
+    document.body.addEventListener(
+      LESSON_END,
+      (event) => {
+        // setGameResult(event.detail as lessonEndData);
+        setGameResult(event);
+        console.log("----------line 100 add event listener------", event);
+      },
+      { once: true }
+    );
     document.body.addEventListener(GAME_END, killGame, { once: true });
     document.body.addEventListener(GAME_EXIT, gameExit, { once: true });
 
     // document.body.addEventListener("problemEnd", onProblemEnd);
   }
-  const saveTempData = async (lessonData:CocosLessonData, isLoved: boolean | undefined) => {
+  const saveTempData = async (
+    lessonData: CocosLessonData,
+    isLoved: boolean | undefined
+  ) => {
     const api = ServiceConfig.getI().apiHandler;
     const courseDocId: string | undefined = state.courseDocId;
     const lesson: Lesson = JSON.parse(state.lesson);
     console.log("🚀 ~ file: CocosGame.tsx:57 ~ init ~ lesson:", lesson);
     console.log("--------lesson data -------", lessonData);
-    console.log("--------score of the lesson",lessonData.score);
+    console.log("--------score of the lesson", lessonData.score);
     const currentStudent = api.currentStudent!;
     const data = lessonData;
     const isStudentLinked = await api.isStudentLinked(currentStudent.docId);
@@ -142,10 +151,7 @@ const CocosGame: React.FC = () => {
       schoolId: schoolId,
       ...data,
     });
-    console.log(
-      "🚀 ~ file: CocosGame.tsx:88 ~ saveTempData ~ result:",
-      result
-    );
+    console.log("🚀 ~ file: CocosGame.tsx:88 ~ saveTempData ~ result:", result);
     let tempAssignmentCompletedIds = localStorage.getItem(
       ASSIGNMENT_COMPLETED_IDS
     );
@@ -168,36 +174,43 @@ const CocosGame: React.FC = () => {
   };
 
   return (
-      <IonPage id="cocos-game-page">
-        <IonContent>
-          <Loading isLoading={isLoading} />
-          {showDialogBox && (
-            <DialogBoxButtons
-              width={"35vw"}
-              height={"25vh"}
-              message={"Do you like the lesson"}
+    <IonPage id="cocos-game-page">
+      <IonContent>
+        <Loading isLoading={isLoading} />
+        {showDialogBox && (
+          <div>
+            <NewScoreCard
+              width={"50vw"}
+              height={"55vh"}
+              title={"Congratulations🎊🎉"}
+              score={gameResult.detail.gameScore}
+              message={"You Completed the Lesson"}
               showDialogBox={showDialogBox}
-              yesText={"Yes"}
-              noText={"No"}
-              handleClose={(e:any) => {
-                setShowDialogBox(false);
-                saveTempData(gameResult.detail, undefined);
-                push();
+              yesText={"Like the Game" }
+              lessonName={gameResult.detail.chapterName}
+              noText={"Continue Playing"}
+              handleClose={(e: any) => {
+                setShowDialogBox(true);
+                // saveTempData(gameResult.detail, undefined);
+                // push();
               }}
-              onYesButtonClicked={(e:any) => {
+              onYesButtonClicked={(e: any) => {
                 setShowDialogBox(false);
                 saveTempData(gameResult.detail, true);
+                console.log("------------------the game result ",gameResult.detail.score);
                 push();
               }}
-              onNoButtonClicked={(e:any) => {
+              onContinueButtonClicked={(e: any) => {
                 setShowDialogBox(false);
-                saveTempData(gameResult.detail, false);
+                saveTempData(gameResult.detail, undefined);
+                console.log("------------------the game result ",gameResult.detail.score);
                 push();
               }}
             />
-          )}
-        </IonContent>
-      </IonPage>
+          </div>
+        )}
+      </IonContent>
+    </IonPage>
   );
 };
 
