7d15e865875c4c2ab27243e6467dca583e3c50a3
Mary-Harshitha <62737530+Mary-Harshitha@users.noreply.github.com>
2023-07-10 16:48:07 +0530

    commit

--- src/services/Firebase.ts
+++ src/services/Firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {FirebaseAnalytics} from "@capacitor-community/firebase-analytics";
+import { FirebaseAnalytics } from "@capacitor-community/firebase-analytics";
 import {
   CACHE_SIZE_UNLIMITED,
   initializeFirestore,
@@ -22,16 +22,16 @@ export const initializeFireBase = () => {
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY!,
-        authDomain: process.env.REACT_APP_AUTH_DOMAIN!,
-        projectId: process.env.REACT_APP_PROJECT_ID!,
-        storageBucket: process.env.REACT_APP_STORAGE_BUCKET!,
-        messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID!,
-        appId: process.env.REACT_APP_APP_ID!,
-        measurementId: process.env.REACT_APP_MEASUREMENT_ID!
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN!,
+    projectId: process.env.REACT_APP_PROJECT_ID!,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET!,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID!,
+    appId: process.env.REACT_APP_APP_ID!,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID!
   };
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig); 
+  const app = initializeApp(firebaseConfig);
   // Initialize Cloud Firestore and get a reference to the service
 
   // const db = getFirestore(app)
