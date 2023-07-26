f1328c72522590ca57a523d4f43162f8e5ce49a0
ALOK-TV <alok@sutara.org>
2023-07-26 16:58:42 +0530

    subjects sorted

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -296,3 +296,10 @@ export const IS_MIGRATION_CHECKED = "isMigrationChecked";
 export const HOT_UPDATE_SERVER = process.env.REACT_APP_HOT_UPDATE_SERVER;
 
 export const COPIED_BUNDLE_FILES_INDEX = "copiedBundleFilesIndex";
+
+export const courseSortIndex = {
+  en: 1,
+  maths: 2,
+  puzzle: 3,
+  hi: 4,
+};
