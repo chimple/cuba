39c7063e56241cabcfa627947ed53e63dec6484d
ALOK-TV <alok@sutara.org>
2023-08-16 21:22:25 +0530

    removed all the unwanted assets

--- src/components/displaySubjects/SelectCourse.tsx
+++ src/components/displaySubjects/SelectCourse.tsx
@@ -21,8 +21,8 @@ const SelectCourse: FC<{
           >
             <div className="course-icon">
               <SelectIconImage
-                localSrc={`courses/chapter_icons/${course.courseCode}.png`}
-                defaultSrc={"courses/" + "maths" + "/icons/" + "maths10.png"}
+                localSrc={`courses/chapter_icons/${course.courseCode}.webp`}
+                defaultSrc={"courses/" + "en" + "/icons/" + "en38.webp"}
                 webSrc={course.thumbnail}
               />
             </div>
