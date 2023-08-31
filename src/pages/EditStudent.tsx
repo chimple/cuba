bb8939c3e35adb747a271362feff6ed205e7cbee
Rajeshreddydari <134476773+Rajeshreddydari@users.noreply.github.com>
2023-08-31 11:53:53 +0530

    Update EditStudent.tsx

--- src/pages/EditStudent.tsx
+++ src/pages/EditStudent.tsx
@@ -140,6 +140,7 @@ const EditStudent = () => {
         await Util.setCurrentStudent(
           student,
           langIndex && languages ? languages[langIndex]?.code : undefined,
+          false,
           false
         );
       }
