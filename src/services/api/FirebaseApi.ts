bec7768519e1d7ed8743e071375813af82e28d47
Rakesh7729 <rakeshkumar@sutara.org>
2023-09-06 15:52:51 +0530

    Master branch - Screen keeps on loading after completing a lesson

--- src/services/api/FirebaseApi.ts
+++ src/services/api/FirebaseApi.ts
@@ -847,6 +847,8 @@ export class FirebaseApi implements ServiceApi {
       console.log("studentProfile", studentProfile);
       if (studentProfile === undefined) return;
       studentProfile.docId = studentId;
+      if (!studentProfile.lessons)
+        studentProfile.lessons = {};
       this._studentResultCache[studentId] = studentProfile;
       return studentProfile;
     } catch (error) {
