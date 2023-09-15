2ed2cd293e3657d1462c4c631ce432db226539cf
Rahul-Chimple <rahul.lanka@sutara.org>
2023-09-15 16:43:08 +0530

    created introduction pages

--- src/components/common/NextButton.tsx
+++ src/components/common/NextButton.tsx
@@ -1,38 +1,23 @@
-import { IonIcon } from '@ionic/react'
-import { t } from 'i18next'
-import { chevronForward } from 'ionicons/icons'
-import "./NextButton.css"
+import { IonIcon } from '@ionic/react';
+import { t } from 'i18next';
+import { chevronForward } from 'ionicons/icons';
+import "./NextButton.css";
+import React from "react";
 
-
- const NextButton: React.FC<{
-  onClicked:React.MouseEventHandler<HTMLButtonElement>,
-  disabled:boolean
-  
-  
-}> = ({onClicked,disabled}) => {
-  return (
-    <button
-    id='common-next-button'
-    disabled={disabled}
-    onClick={onClicked}
-    // style={{
-    //   position:'absolute'
-    // }} 
-      >
-        {t("Next")}
-        <IonIcon
-          className="arrow-icon"
-          slot="end"
-          icon={chevronForward}
-        ></IonIcon>
-    
-    </button>
-  )
+interface NextButtonProps {
+  onClicked: React.MouseEventHandler<HTMLButtonElement>;
+  disabled: boolean;
+  children?: React.ReactNode;
 }
 
-export default NextButton
+const NextButton: React.FC<NextButtonProps> = ({ onClicked, disabled, children }) => {
+  return (
+    <button id='common-next-button' disabled={disabled} onClick={onClicked}>
+      {children} 
+      {t("Next")}
+      <IonIcon className="arrow-icon" slot="end" icon={chevronForward}></IonIcon>
+    </button>
+  );
+}
 
-
-
-
-
+export default NextButton;
