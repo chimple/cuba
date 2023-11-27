4df155ab438da3c3b9525552b9b7f2b8380ba44d
Skanda <skanda@sutara.org>
2023-11-27 19:28:27 +0530

    added capacitor-community text-to-speech plugin

--- src/components/animation/AudioButtonComponent.tsx
+++ src/components/animation/AudioButtonComponent.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { useAudioPlayer } from './animationUtils';
-import { PiSpeakerHighBold } from 'react-icons/pi';
+import React from "react";
+import { useAudioPlayer, useTtsAudioPlayer } from "./animationUtils";
+import { PiSpeakerHighBold } from "react-icons/pi";
+import { TextToSpeech } from "@capacitor-community/text-to-speech";
+import { Capacitor } from "@capacitor/core";
 
 interface AudioButtonProps {
   style: React.CSSProperties;
@@ -8,11 +10,48 @@ interface AudioButtonProps {
 }
 
 function AudioButtonComponent({ style, audioSrc }: AudioButtonProps) {
-  const { playAudio, playing } = useAudioPlayer(audioSrc);
+  // const { playAudio, playing } = useAudioPlayer(audioSrc);
+  const audioText = "नमस्ते! चिम्पल में आपका स्वागत है";
+  // const audioText = "Hi! welcome to Chimple";
+  const {
+    speak,
+    stop,
+    playing,
+    getSupportedLanguages,
+    getSupportedVoices,
+    isLanguageSupported,
+  } = useTtsAudioPlayer(audioText, "hi-IN");
 
   return (
     <div>
-      <PiSpeakerHighBold style={style} onClick={playAudio} />
+      <PiSpeakerHighBold
+        style={style}
+        onClick={async () => {
+          if (!playing) {
+            if (Capacitor.isNativePlatform()) {
+              const inst = await TextToSpeech.openInstall();
+              console.log("await TextToSpeech.openInstall(); =", inst);
+              alert(
+                "await TextToSpeech.openInstall(); =" + JSON.stringify(inst)
+              );
+            }
+            const languages = await getSupportedLanguages();
+            console.log("await getSupportedLanguages() =", languages);
+            const voices = await getSupportedVoices();
+            console.log("const voices = await getSupportedVoices();", voices);
+            const isSupported = await isLanguageSupported("kn-IN");
+            console.log(
+              "const isSupported = await isLanguageSupported(kn-IN);",
+              isSupported
+            );
+            // alert("const languages =" + JSON.stringify(languages));
+            // alert(
+            //   "const languages.languages =" + JSON.stringify(languages.languages)
+            // );
+            await speak();
+          }
+        }}
+      />
     </div>
   );
 }
--- src/components/animation/animationUtils.tsx
+++ src/components/animation/animationUtils.tsx
@@ -1,37 +1,96 @@
-import { useState, useEffect } from 'react';
+import { TextToSpeech } from "@capacitor-community/text-to-speech";
+import { useState, useEffect } from "react";
 
 export function useAudioPlayer(audioSrc: string) {
-    const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
-    const [playing, setPlaying] = useState<boolean>(false);
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+  const [playing, setPlaying] = useState<boolean>(false);
 
-    useEffect(() => {
-        if (audio) {
-            audio.addEventListener('ended', () => setPlaying(false));
-            return () => {
-                audio.removeEventListener('ended', () => setPlaying(false));
-            };
-        }
-    }, [audio]);
-
-    const playAudio = () => {
-        const newAudio = new Audio(audioSrc);
-        newAudio.onloadedmetadata = () => {
-            console.log("Audio duration: ", newAudio.duration);
-            setPlaying(true);
-            if (!playing) {
-                newAudio.play();
-                console.log("Audio is playing: ", audioSrc);
-            }
-        };
-
-        newAudio.load();
-        setAudio(newAudio);
-    };
-    const pauseAudio = () => {
-        if (audio && !playing) {
-            audio.pause();
-        }
+  useEffect(() => {
+    if (audio) {
+      audio.addEventListener("ended", () => setPlaying(false));
+      return () => {
+        audio.removeEventListener("ended", () => setPlaying(false));
+      };
     }
+  }, [audio]);
 
-    return { playAudio, playing , pauseAudio};
+  const playAudio = () => {
+    const newAudio = new Audio(audioSrc);
+    newAudio.onloadedmetadata = () => {
+      console.log("Audio duration: ", newAudio.duration);
+      setPlaying(true);
+      if (!playing) {
+        newAudio.play();
+        console.log("Audio is playing: ", audioSrc);
+      }
+    };
+
+    newAudio.load();
+    setAudio(newAudio);
+  };
+  const pauseAudio = () => {
+    if (audio && !playing) {
+      audio.pause();
+    }
+  };
+
+  return { playAudio, playing, pauseAudio };
+}
+
+export function useTtsAudioPlayer(audioText: string, audioLang: string) {
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+  const [playing, setPlaying] = useState<boolean>(false);
+
+  const speak = async () => {
+    try {
+      console.log("speak method called ", playing, !playing);
+
+      setPlaying(true);
+      if (!playing) {
+        await TextToSpeech.speak({
+          text: audioText, //"नमस्ते चिम्पल, आप सभी का स्वागत है",
+          lang: audioLang, //"hi-IN",
+          rate: 1.0,
+          pitch: 1.0,
+          volume: 1.0,
+          category: "ambient",
+        }).then(() => {
+          setPlaying(false);
+        });
+        console.log("Audio is playing: ", audioText);
+      }
+    } catch (error) {
+      console.log("TTS speech failed ", playing, error);
+    }
+  };
+
+  const stop = async () => {
+    if (!playing) {
+      await TextToSpeech.stop();
+    }
+  };
+
+  const getSupportedLanguages = async () => {
+    const lang = await TextToSpeech.getSupportedLanguages();
+    return lang.languages;
+  };
+
+  const getSupportedVoices = async () => {
+    const voices = await TextToSpeech.getSupportedVoices();
+    return voices.voices;
+  };
+
+  const isLanguageSupported = async (lang: string) => {
+    const isSupported = await TextToSpeech.isLanguageSupported({ lang });
+    return isSupported.supported;
+  };
+
+  return {
+    speak,
+    stop,
+    playing,
+    getSupportedLanguages,
+    getSupportedVoices,
+    isLanguageSupported,
+  };
 }
