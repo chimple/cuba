35ededc78adfd48b05c439b5b9e17e3be593ae01
ALOK-TV <alok@sutara.org>
2024-02-01 08:18:13 +0530

    audio button issue fixed

--- src/pages/Parent.tsx
+++ src/pages/Parent.tsx
@@ -43,8 +43,8 @@ import DropDown from "../components/DropDown";
 const Parent: React.FC = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [currentHeader, setCurrentHeader] = useState<any>(undefined);
-  const [soundFlag, setSoundFlag] = useState<boolean>();
-  const [musicFlag, setMusicFlag] = useState<boolean>();
+  const [soundFlag, setSoundFlag] = useState<number>();
+  const [musicFlag, setMusicFlag] = useState<number>();
   const [userProfile, setUserProfile] = useState<User[]>([]);
   const [tabIndex, setTabIndex] = useState<any>();
 
@@ -80,11 +80,10 @@ const Parent: React.FC = () => {
     getStudentProfile();
   }, [reloadProfiles]);
 
-
   async function getStudentProfile() {
     console.log("getStudentProfile");
-    const userProfilePromise: User[] = await
-      ServiceConfig.getI().apiHandler.getParentStudentProfiles();
+    const userProfilePromise: User[] =
+      await ServiceConfig.getI().apiHandler.getParentStudentProfiles();
     let finalUser: any[] = [];
     for (let i = 0; i < MAX_STUDENTS_ALLOWED; i++) {
       finalUser.push(userProfilePromise[i]);
@@ -97,7 +96,7 @@ const Parent: React.FC = () => {
     if (parentUser != undefined) {
       const currMode = await schoolUtil.getCurrMode();
       setStudentMode(currMode);
-      console.log("User ", parentUser?.musicFlag!);
+      console.log("User ", parentUser?.musicOff!);
       const sound = Util.getCurrentSound();
       const music = Util.getCurrentMusic();
       setSoundFlag(sound);
@@ -164,7 +163,7 @@ const Parent: React.FC = () => {
               showText={true}
               setReloadProfiles={setReloadProfiles}
               profiles={userProfile}
-              studentCurrMode= {studentMode}
+              studentCurrMode={studentMode}
             />
           );
         })}
@@ -223,15 +222,15 @@ const Parent: React.FC = () => {
               flag={soundFlag!}
               title={t("Sound")}
               onIonChangeClick={async (v) => {
-                console.log("ion change value ", v.detail?.checked);
-                setSoundFlag(v.detail?.checked);
+                console.log("ion change value ", v.detail?.checked ? 0 : 1);
+                setSoundFlag(v.detail?.checked ? 0 : 1);
                 const currentUser =
                   await ServiceConfig.getI().authHandler.getCurrentUser();
-                Util.setCurrentSound(v.detail?.checked);
+                Util.setCurrentSound(v.detail?.checked ? 0 : 1);
                 if (currentUser) {
                   ServiceConfig.getI().apiHandler.updateSoundFlag(
                     currentUser,
-                    v.detail?.checked
+                    v.detail?.checked ? 0 : 1
                   );
                 }
               }}
@@ -241,15 +240,15 @@ const Parent: React.FC = () => {
               flag={musicFlag!}
               title={t("Music")}
               onIonChangeClick={async (v) => {
-                console.log("ion change value ", v.detail?.checked);
-                setMusicFlag(v.detail?.checked);
+                console.log("ion change value ", v.detail?.checked ? 0 : 1);
+                setMusicFlag(v.detail?.checked ? 0 : 1);
                 const currentUser =
                   await ServiceConfig.getI().authHandler.getCurrentUser();
-                Util.setCurrentMusic(v.detail?.checked);
+                Util.setCurrentMusic(v.detail?.checked ? 0 : 1);
                 if (currentUser) {
                   ServiceConfig.getI().apiHandler.updateMusicFlag(
                     currentUser,
-                    v.detail?.checked
+                    v.detail?.checked ? 0 : 1
                   );
                 }
               }}
@@ -335,7 +334,7 @@ const Parent: React.FC = () => {
                   title="YouTube video player"
                   // frameborder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                // allowfullscreen
+                  // allowfullscreen
                 ></iframe>
               </div>
             </div>
@@ -402,9 +401,13 @@ const Parent: React.FC = () => {
   }
   function faqUI() {
     return (
-      <div id="faq-page"
+      <div
+        id="faq-page"
         onClick={() => {
-          window.open('https://www.chimple.org/in-school-guide-for-teachers', "_system");
+          window.open(
+            "https://www.chimple.org/in-school-guide-for-teachers",
+            "_system"
+          );
         }}
       >
         <p>{t("Please Visit Our Website")}</p>
