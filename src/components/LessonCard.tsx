b2383dca96a3a98d5824fbaa40a19af176fa0da5
renuka07 <133939590+renukaj07@users.noreply.github.com>
2024-02-25 10:48:11 +0530

    displaying homework assigned date on lesson card

--- src/components/LessonCard.tsx
+++ src/components/LessonCard.tsx
@@ -40,6 +40,7 @@ const LessonCard: React.FC<{
   startIndex: number;
   showChapterName: boolean;
   downloadButtonLoading?: boolean;
+  showDate?: boolean;
 }> = ({
   width,
   height,
@@ -57,6 +58,7 @@ const LessonCard: React.FC<{
   startIndex,
   showChapterName = false,
   downloadButtonLoading,
+  showDate,
 }) => {
   const history = useHistory();
   const [showImage, setShowImage] = useState(true);
@@ -64,7 +66,7 @@ const LessonCard: React.FC<{
   // const [subject, setSubject] = useState<Subject>();
   const [currentCourse, setCurrentCourse] = useState<Course>();
   const { online, presentToast } = useOnlineOfflineErrorMessageHandler();
-
+  const [date, setDate] = useState<Date>();
   const hideImg = (event: any) => {
     setShowImage(false);
   };
@@ -72,6 +74,7 @@ const LessonCard: React.FC<{
   useEffect(() => {
     // getSubject();
     getCurrentCourse();
+    getDate();
   }, [lesson]);
 
   // const getSubject = async () => {
@@ -85,7 +88,13 @@ const LessonCard: React.FC<{
   //   }
   //   setSubject(subject);
   // };
-
+  const getDate = () => {
+    const res = lesson.assignment?.updatedAt;
+    if (!!res) {
+      const dateObj = res?.toDate();
+      setDate(dateObj);
+    }
+  };
   const getCurrentCourse = async () => {
     const currentStudent = Util.getCurrentStudent();
     if (!currentStudent) {
@@ -123,208 +132,230 @@ const LessonCard: React.FC<{
   }, []);
 
   return (
-    <IonCard
-      id="lesson-card"
-      style={{
-        width: width,
-        height: "auto",
-      }}
-      onClick={async () => {
-        if (isUnlocked) {
-          // if (
-          //   lesson.chapter.course.isCourseMapped &&
-          //   lesson.orig_course_id != undefined &&
-          //   lesson.orig_chapter_id != undefined &&
-          //   lesson.orig_lesson_id != undefined
-          // ) {
-          //   const parmas = `?courseid=${lesson.orig_course_id}&chapterid=${lesson.orig_chapter_id}&lessonid=${lesson.orig_lesson_id}`;
-          //   console.log("parmas", parmas);
-          //   history.push(PAGES.GAME + parmas, {
-          //     url: "chimple-lib/index.html" + parmas,
-          //     lessonId: lesson.orig_lesson_id,
-          //     lesson: lesson,
-          //     from: history.location.pathname,
-          //   });
-          // } else {
-          // console.log("LessonCard course: subject,", subject);
-          console.log("LessonCard course: course,", currentCourse);
-          if (lesson.pluginType === COCOS) {
-            const parmas = `?courseid=${lesson.cocosSubjectCode}&chapterid=${lesson.cocosChapterCode}&lessonid=${lesson.id}`;
-            console.log(
-              "🚀 ~ file: LessonCard.tsx:73 ~ parmas:",
-              parmas,
-              Lesson.toJson(lesson)
-            );
-            history.replace(PAGES.GAME + parmas, {
-              url: "chimple-lib/index.html" + parmas,
-              lessonId: lesson.id,
-              courseDocId: course?.docId ?? lesson?.assignment?.course?.id,
-              course: JSON.stringify(Course.toJson(currentCourse!)),
-              lesson: JSON.stringify(Lesson.toJson(lesson)),
-              from: history.location.pathname + `?${CONTINUE}=true`,
-            });
-          } else if (
-            !!lesson?.assignment?.docId &&
-            lesson.pluginType === LIVE_QUIZ
-          ) {
-            if (!online) {
-              presentToast({
-                message: t(`Device is offline`),
-                color: "danger",
-                duration: 3000,
-                position: "bottom",
-                buttons: [
-                  {
-                    text: "Dismiss",
-                    role: "cancel",
-                  },
-                ],
-              });
-              return;
-            }
-            history.replace(
-              PAGES.LIVE_QUIZ_JOIN +
-                `?assignmentId=${lesson?.assignment?.docId}`,
-              {
-                assignment: JSON.stringify(lesson?.assignment),
-              }
-            );
-          }
-        }
-      }}
-      // disabled={!isUnlocked}
-    >
-      <div
+    <>
+      <div className="assigned-date-div">
+        {!!showDate && lesson.assignment ? (
+          <div id="lesson-card-date">
+            <p>
+              {t("Assigned") + ": "}
+              <b>
+                {!!date &&
+                  (() => {
+                    const day = date.getDate().toString().padStart(2, "0");
+                    const month = (date.getMonth() + 1)
+                      .toString()
+                      .padStart(2, "0");
+                    const year = date.getFullYear();
+                    return `${day}/${month}/${year}`;
+                  })()}
+              </b>
+            </p>
+          </div>
+        ) : null}
+      </div>
+
+      <IonCard
+        id="lesson-card"
         style={{
-          display: "grid",
+          width: width,
+          height: "auto",
+        }}
+        onClick={async () => {
+          if (isUnlocked) {
+            // if (
+            //   lesson.chapter.course.isCourseMapped &&
+            //   lesson.orig_course_id != undefined &&
+            //   lesson.orig_chapter_id != undefined &&
+            //   lesson.orig_lesson_id != undefined
+            // ) {
+            //   const parmas = `?courseid=${lesson.orig_course_id}&chapterid=${lesson.orig_chapter_id}&lessonid=${lesson.orig_lesson_id}`;
+            //   console.log("parmas", parmas);
+            //   history.push(PAGES.GAME + parmas, {
+            //     url: "chimple-lib/index.html" + parmas,
+            //     lessonId: lesson.orig_lesson_id,
+            //     lesson: lesson,
+            //     from: history.location.pathname,
+            //   });
+            // } else {
+            // console.log("LessonCard course: subject,", subject);
+            console.log("LessonCard course: course,", currentCourse);
+            if (lesson.pluginType === COCOS) {
+              const parmas = `?courseid=${lesson.cocosSubjectCode}&chapterid=${lesson.cocosChapterCode}&lessonid=${lesson.id}`;
+              console.log(
+                "🚀 ~ file: LessonCard.tsx:73 ~ parmas:",
+                parmas,
+                Lesson.toJson(lesson)
+              );
+              history.replace(PAGES.GAME + parmas, {
+                url: "chimple-lib/index.html" + parmas,
+                lessonId: lesson.id,
+                courseDocId: course?.docId ?? lesson?.assignment?.course?.id,
+                course: JSON.stringify(Course.toJson(currentCourse!)),
+                lesson: JSON.stringify(Lesson.toJson(lesson)),
+                from: history.location.pathname + `?${CONTINUE}=true`,
+              });
+            } else if (
+              !!lesson?.assignment?.docId &&
+              lesson.pluginType === LIVE_QUIZ
+            ) {
+              if (!online) {
+                presentToast({
+                  message: t(`Device is offline`),
+                  color: "danger",
+                  duration: 3000,
+                  position: "bottom",
+                  buttons: [
+                    {
+                      text: "Dismiss",
+                      role: "cancel",
+                    },
+                  ],
+                });
+                return;
+              }
+              history.replace(
+                PAGES.LIVE_QUIZ_JOIN +
+                  `?assignmentId=${lesson?.assignment?.docId}`,
+                {
+                  assignment: JSON.stringify(lesson?.assignment),
+                }
+              );
+            }
+          }
         }}
       >
         <div
           style={{
-            background: lessonCardColor,
-            borderRadius: "7vh",
-            width: width,
-            height: height,
             display: "grid",
-            justifyContent: "center",
-            alignItems: "center",
-            gridArea: "1/1",
           }}
-          color={lessonCardColor}
         >
-          <div id="lesson-card-homework-icon">
-            {lesson.assignment !== undefined &&
-              (!(TYPE in lesson.assignment) ||
-              lesson.assignment.type !== LIVE_QUIZ ? (
-                <div>
-                  <img
-                    src="assets/icons/homework_icon.svg"
-                    className="lesson-card-homework-indicator"
-                    alt="Homework Icon"
-                  />
-                </div>
-              ) : (
-                <div>
-                  <img
-                    src="/assets/icons/quiz_icon.svg"
-                    className="lesson-card-homework-indicator"
-                    alt="Quiz Icon"
-                  />
-                </div>
-              ))}
-          </div>
-
-          {showSubjectName && currentCourse?.title ? (
-            <div id="lesson-card-subject-name">
-              <p>
-                {currentCourse?.title}
-                {/* {subject.title==="English"?subject.title:t(subject.title)} */}
-              </p>
-            </div>
-          ) : null}
-          <div className="pattern">
-            <SelectIconImage
-              localSrc={
-                // this is for lesson card background
-                "courses/" + "sl_en1_mp" + "/icons/" + "ChallengePattern.png"
-              }
-              defaultSrc={
-                "courses/" + "sl_en1_mp" + "/icons/" + "ChallengePattern.png"
-              }
-              webSrc={
-                "https://firebasestorage.googleapis.com/v0/b/cuba-stage.appspot.com/o/lesson_thumbnails%2FlessonCaredPattern%2FChallengePattern.png?alt=media&token=be64aec1-f70f-43c3-95de-fd4b1afe5806"
-              }
-            />
-          </div>
-
-          <div id="lesson-card-image">
-            <SelectIconImage
-              localSrc={
-                "courses/" +
-                lesson.cocosSubjectCode +
-                "/icons/" +
-                lesson.id +
-                ".webp"
-              }
-              defaultSrc={"courses/" + "en" + "/icons/" + "en38.webp"}
-              webSrc={lesson.thumbnail}
-              imageWidth={"100%"}
-              imageHeight={"100%"}
-            />
-            {!isUnlocked ? (
-              <div id="lesson-card-status-icon">
-                <img
-                  id="lesson-card-status-icon1"
-                  loading="lazy"
-                  src="assets/icons/Lock_icon.svg"
-                  alt="assets/icons/Lock_icon.svg"
-                />
-              </div>
-            ) : isPlayed ? (
-              showScoreCard ? (
-                <div>
-                  <div id="lesson-card-score">
-                    <LessonCardStarIcons score={score}></LessonCardStarIcons>
+          <div
+            style={{
+              background: lessonCardColor,
+              borderRadius: "7vh",
+              width: width,
+              height: height,
+              display: "grid",
+              justifyContent: "center",
+              alignItems: "center",
+              gridArea: "1/1",
+            }}
+            color={lessonCardColor}
+          >
+            <div id="lesson-card-homework-icon">
+              {lesson.assignment !== undefined &&
+                (!(TYPE in lesson.assignment) ||
+                lesson.assignment.type !== LIVE_QUIZ ? (
+                  <div>
+                    <img
+                      src="assets/icons/homework_icon.svg"
+                      className="lesson-card-homework-indicator"
+                      alt="Homework Icon"
+                    />
                   </div>
+                ) : (
+                  <div>
+                    <img
+                      src="/assets/icons/quiz_icon.svg"
+                      className="lesson-card-homework-indicator"
+                      alt="Quiz Icon"
+                    />
+                  </div>
+                ))}
+            </div>
 
-                  {/* {isLoved && <LovedIcon isLoved={isLoved} hasChapterTitle={!!lesson.chapterTitle && showChapterName} />} */}
+            {showSubjectName && currentCourse?.title ? (
+              <div id="lesson-card-subject-name">
+                <p>
+                  {currentCourse?.title}
+                  {/* {subject.title==="English"?subject.title:t(subject.title)} */}
+                </p>
+              </div>
+            ) : null}
+            <div className="pattern">
+              <SelectIconImage
+                localSrc={
+                  // this is for lesson card background
+                  "courses/" + "sl_en1_mp" + "/icons/" + "ChallengePattern.png"
+                }
+                defaultSrc={
+                  "courses/" + "sl_en1_mp" + "/icons/" + "ChallengePattern.png"
+                }
+                webSrc={
+                  "https://firebasestorage.googleapis.com/v0/b/cuba-stage.appspot.com/o/lesson_thumbnails%2FlessonCaredPattern%2FChallengePattern.png?alt=media&token=be64aec1-f70f-43c3-95de-fd4b1afe5806"
+                }
+              />
+            </div>
+
+            <div id="lesson-card-image">
+              <SelectIconImage
+                localSrc={
+                  "courses/" +
+                  lesson.cocosSubjectCode +
+                  "/icons/" +
+                  lesson.id +
+                  ".webp"
+                }
+                defaultSrc={"courses/" + "en" + "/icons/" + "en38.webp"}
+                webSrc={lesson.thumbnail}
+                imageWidth={"100%"}
+                imageHeight={"100%"}
+              />
+              {!isUnlocked ? (
+                <div id="lesson-card-status-icon">
+                  <img
+                    id="lesson-card-status-icon1"
+                    loading="lazy"
+                    src="assets/icons/Lock_icon.svg"
+                    alt="assets/icons/Lock_icon.svg"
+                  />
                 </div>
-              ) : (
-                <></>
-              )
-            ) : (
-              <div />
-            )}
-          </div>
+              ) : isPlayed ? (
+                showScoreCard ? (
+                  <div>
+                    <div id="lesson-card-score">
+                      <LessonCardStarIcons score={score}></LessonCardStarIcons>
+                    </div>
 
-          {/* {isLoved && <LovedIcon isLoved={isLoved} hasChapterTitle={!!lesson.chapterTitle && showChapterName} />} */}
-        </div>
-        <div className="lesson-download-button-container">
-          <DownloadLesson
-            lessonId={lesson.id}
-            downloadButtonLoading={downloadButtonLoading}
-          />
-        </div>
-        {isLoved && (
-          <LovedIcon
-            isLoved={isLoved}
-            hasChapterTitle={!!lesson.chapterTitle && showChapterName}
-          />
-        )}
-      </div>
-      <div>
-        {showText ? (
-          <p id={`lesson-card-name${isLoved ? "-fav-icon" : ""}`}>
-            {t(lesson?.title)}
-          </p>
-        ) : null}
-        {showChapterName && lesson.chapterTitle && (
-          <div id={`chapter-title${isLoved ? "-fav-icon" : ""}`}>
-            {lesson.chapterTitle}
+                    {/* {isLoved && <LovedIcon isLoved={isLoved} hasChapterTitle={!!lesson.chapterTitle && showChapterName} />} */}
+                  </div>
+                ) : (
+                  <></>
+                )
+              ) : (
+                <div />
+              )}
+            </div>
+
+            {/* {isLoved && <LovedIcon isLoved={isLoved} hasChapterTitle={!!lesson.chapterTitle && showChapterName} />} */}
           </div>
-        )}
-      </div>
-    </IonCard>
+          <div className="lesson-download-button-container">
+            <DownloadLesson
+              lessonId={lesson.id}
+              downloadButtonLoading={downloadButtonLoading}
+            />
+          </div>
+          {isLoved && (
+            <LovedIcon
+              isLoved={isLoved}
+              hasChapterTitle={!!lesson.chapterTitle && showChapterName}
+            />
+          )}
+        </div>
+        <div>
+          {showText ? (
+            <p id={`lesson-card-name${isLoved ? "-fav-icon" : ""}`}>
+              {t(lesson?.title)}
+            </p>
+          ) : null}
+          {showChapterName && lesson.chapterTitle && (
+            <div id={`chapter-title${isLoved ? "-fav-icon" : ""}`}>
+              {lesson.chapterTitle}
+            </div>
+          )}
+        </div>
+      </IonCard>
+    </>
   );
 };
 
