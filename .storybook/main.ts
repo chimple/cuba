29f0752d90ba3c10467d98f644c8a587381d39c7
Mary-Harshitha <62737530+Mary-Harshitha@users.noreply.github.com>
2024-05-07 18:43:59 +0530

    i18n support for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,6 +9,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "storybook-react-i18next",
   ],
   framework: {
     name: "@storybook/react-webpack5",
