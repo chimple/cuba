d4f41a51382053b2aa3be61d5f9bdf91df484ffd
Vinay Reddy <vinay@sutara.org>
2024-06-04 17:42:51 +0530

    Update SqliteApi.ts

--- src/services/api/SqliteApi.ts
+++ src/services/api/SqliteApi.ts
@@ -35,7 +35,7 @@ export class SqliteApi implements ServiceApi {
   private _db: SQLiteDBConnection | undefined;
   private _sqlite: SQLiteConnection | undefined;
   private DB_NAME = "db_issue10";
-  private DB_VERSION = 2;
+  private DB_VERSION = 1;
   private _serverApi: SupabaseApi;
   private _currentMode: MODES;
   private _currentStudent: TableTypes<"user"> | undefined;
