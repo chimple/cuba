aa16957f416d5fc34778413d8c53725e7005620e
Bhanu <62737055+bhanugummana@users.noreply.github.com>
2024-06-20 15:49:22 +0530

    jionclass header issue

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -131,6 +131,9 @@ const Home: FC = () => {
     const urlParams = new URLSearchParams(window.location.search);
     if (urlParams.get("page") === PAGES.JOIN_CLASS) {
       setCurrentHeader(HOMEHEADERLIST.ASSIGNMENT);
+      setTimeout(() => {
+        setCurrentHeader(HOMEHEADERLIST.ASSIGNMENT);
+      }, 500);
     } else if (urlParams.get("page") === PAGES.LIVE_QUIZ) {
       if (linked) setCurrentHeader(HOMEHEADERLIST.LIVEQUIZ);
       else setCurrentHeader(HOMEHEADERLIST.ASSIGNMENT);
