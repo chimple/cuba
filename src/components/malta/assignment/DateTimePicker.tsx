355c97622e5b0446269677afd4758ef8d68d5405
Skanda <skanda@sutara.org>
2024-11-18 10:04:50 +0530

    Fixed Assignment issue

--- src/components/malta/assignment/DateTimePicker.tsx
+++ src/components/malta/assignment/DateTimePicker.tsx
@@ -3,17 +3,20 @@ import { FC } from "react";
 
 interface DateTimePickerProps {
   date: string;
+  onClickChange?: (e: any) => void;
 }
 
-const DateTimePicker: FC<DateTimePickerProps> = ({ date }) => {
+const DateTimePicker: FC<DateTimePickerProps> = ({ date, onClickChange }) => {
   return (
     <>
       <IonDatetimeButton datetime={date}></IonDatetimeButton>
       <IonModal keepContentsMounted={true}>
         <IonDatetime
+          color={"white"}
           id={date}
           presentation="date"
           value={date}
+          onChange={onClickChange}
         ></IonDatetime>
       </IonModal>
     </>
