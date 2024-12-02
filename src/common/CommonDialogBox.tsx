0d34d763966991e0cd3f940e129edbcbaae16792
renuka07 <133939590+renukaj07@users.noreply.github.com>
2024-12-02 17:43:30 +0530

    api to add and delete a school users

--- src/common/CommonDialogBox.tsx
+++ src/common/CommonDialogBox.tsx
@@ -1,44 +1,54 @@
-import { FC, useState } from "react";
-import { IonAlert } from "@ionic/react";
+import { FC } from "react";
+import { IonAlert, AlertButton } from "@ionic/react";
 import { t } from "i18next";
 import "./CommonDialogBox.css";
 
 const CommonDialogBox: FC<{
   message: string;
   showConfirmFlag: boolean;
-  leftButtonText: string;
-  leftButtonHandler: () => void;
+  header?: string;
+  leftButtonText?: string;
+  leftButtonHandler?: () => void;
   onDidDismiss: () => void;
-  rightButtonText: string;
-  rightButtonHandler: () => void;
+  rightButtonText?: string;
+  rightButtonHandler?: () => void;
 }> = ({
   message,
   showConfirmFlag,
+  header,
   leftButtonText,
   leftButtonHandler,
   onDidDismiss,
   rightButtonText,
   rightButtonHandler,
 }) => {
+  const buttons: AlertButton[] = [];
+
+  if (leftButtonText) {
+    buttons.push({
+      text: t(leftButtonText),
+      cssClass: "custom-dailog-alert-delete-button",
+      handler: leftButtonHandler,
+    });
+  }
+
+  if (rightButtonText) {
+    buttons.push({
+      text: t(rightButtonText),
+      cssClass: "custom-dailog-alert-cancel-button",
+      handler: rightButtonHandler,
+    });
+  }
+
   return (
     <div>
       <IonAlert
         isOpen={showConfirmFlag}
+        header={header}
         onDidDismiss={onDidDismiss}
         cssClass="custom-dailog-alert"
         message={t(message) || ""}
-        buttons={[
-          {
-            text: t(leftButtonText),
-            cssClass: "custom-dailog-alert-delete-button",
-            handler: leftButtonHandler,
-          },
-          {
-            text: t(rightButtonText),
-            cssClass: "custom-dailog-alert-cancel-button",
-            handler: rightButtonHandler,
-          },
-        ]}
+        buttons={buttons}
       />
     </div>
   );
