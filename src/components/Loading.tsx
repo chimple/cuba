dc94c79df233819127d4f0fe8feca7980a2086a4
arahman-2k24 <abdul.rahmnan@amiti.in>
2024-12-24 12:37:42 +0530

    added initialLogin optional prop to pass msg during login

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,16 +1,18 @@
 import { IonLoading, LoadingOptions } from "@ionic/react";
 import React from "react";
 import "./Loading.css";
+import { t } from "i18next";
 
 interface LoadingProps extends LoadingOptions {
   isLoading: boolean;
   msg?: string;
+  initialLogin?: boolean;
 }
 
 const Loading: React.FC<LoadingProps> = ({
   isLoading,
   msg = "",
-
+  initialLogin
 }) => {
   return isLoading ? (
     <div>
@@ -18,7 +20,7 @@ const Loading: React.FC<LoadingProps> = ({
         isOpen={isLoading}
         spinner={null}
         message={
-          `<img class="loading" src="assets/loading.gif"></img> <br/><p>${msg}</p>`
+          `<img class="loading" src="assets/loading.gif"></img> <br/><p class="msg">${ initialLogin ? t("Please wait.....Login in progress. This may take a moment.") : ""}</p>`
         }
       />
     </div>
