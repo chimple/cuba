3ba0fe4cdccb1fb01e071ac5d0588418759be547
Rahul-Chimple <rahul.lanka@sutara.org>
2025-01-29 23:38:00 +0530

    navigating to school mode if autouser is there after login

--- src/services/api/ApiHandler.ts
+++ src/services/api/ApiHandler.ts
@@ -879,6 +879,11 @@ export class ApiHandler implements ServiceApi {
   createClassCode(classId: string): Promise<number> {
     return this.s.createClassCode(classId);
   }
+  getSchoolsWithRoleAutouser(
+    schoolIds: string[]
+  ): Promise<TableTypes<"school">[] | undefined> {
+    return this.s.getSchoolsWithRoleAutouser(schoolIds);
+  }
   getPrincipalsForSchool(
     schoolId: string
   ): Promise<TableTypes<"user">[] | undefined> {
