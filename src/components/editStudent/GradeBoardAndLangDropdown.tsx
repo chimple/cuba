49bf62565b2eeaaaf64cf718dd61a0b9d8cd8408
Manish Joshi <manish.joshi@sutara.org>
2025-02-17 16:45:53 +0530

    Language order preserved same for dropdowns
    Language Order preserved same dropdowns in AppLangSelection, Parent, EditStudent

--- src/components/editStudent/GradeBoardAndLangDropdown.tsx
+++ src/components/editStudent/GradeBoardAndLangDropdown.tsx
@@ -10,7 +10,11 @@ import DropDown from "../DropDown";
 const GradeBoardAndLangDropdown: FC<{
   boards: Curriculum[] | undefined;
   grades: Grade[] | undefined;
-  languages: Language[] | undefined;
+  languages: {
+    title: string;
+    code: string;
+    docId: string;
+  }[] | undefined;
   onBoardChange: (event: string) => void;
   onGradeChange: (event: string) => void;
   onLangChange: (event: string) => void;
@@ -28,57 +32,57 @@ const GradeBoardAndLangDropdown: FC<{
   currentlySelectedGrade,
   currentlySelectedLang,
 }) => {
-  const boardOptions = boards?.map((cur) => ({
-    displayName: cur.title,
-    id: cur.docId,
-  }));
-  const boardOptions1 = boards?.map((cur) => cur.title);
-  const gradeOptions = grades?.map((cur) => ({
-    displayName: cur.title,
-    id: cur.docId,
-  }));
-  const languageOptions = languages?.map((cur) => ({
-    displayName: cur.title,
-    id: cur.docId,
-  }));
-  return (
-    <div className="dropdown-header">
-      <div id="drop-down-board">
-        <p id="drop-down-head">{t('Board')}</p>
-           <DropDown
-      currentValue={currentlySelectedBoard!}
-        // currentlySelected={currentlySelectedBoard}
-        placeholder={t("Select").toString()}
-        onValueChange={onBoardChange}
+    const boardOptions = boards?.map((cur) => ({
+      displayName: cur.title,
+      id: cur.docId,
+    }));
+    const boardOptions1 = boards?.map((cur) => cur.title);
+    const gradeOptions = grades?.map((cur) => ({
+      displayName: cur.title,
+      id: cur.docId,
+    }));
+    const languageOptions = languages?.map((cur) => ({
+      displayName: cur.title,
+      id: cur.docId,
+    }));
+    return (
+      <div className="dropdown-header">
+        <div id="drop-down-board">
+          <p id="drop-down-head">{t('Board')}</p>
+          <DropDown
+            currentValue={currentlySelectedBoard!}
+            // currentlySelected={currentlySelectedBoard}
+            placeholder={t("Select").toString()}
+            onValueChange={onBoardChange}
         optionList={boardOptions??[]}
-        width="26vw"
-        // options={boardOptions ?? []}
-        // onDropdownChange={onBoardChange}
-      />
-      </div>
-      <div id="drop-down-grade">
-      <p id="drop-down-head">{t('Grade')}</p>
-      <DropDown
-       currentValue={currentlySelectedGrade!}
-       
-       placeholder={t("Select").toString()}
-       onValueChange={onGradeChange}
+            width="26vw"
+          // options={boardOptions ?? []}
+          // onDropdownChange={onBoardChange}
+          />
+        </div>
+        <div id="drop-down-grade">
+          <p id="drop-down-head">{t('Grade')}</p>
+          <DropDown
+            currentValue={currentlySelectedGrade!}
+
+            placeholder={t("Select").toString()}
+            onValueChange={onGradeChange}
        optionList={gradeOptions??[]}
-       width="26vw"
-      />
-      </div>
-      <div id="drop-down-Medium-of-instruction">
-        <p id="drop-down-head">{t('Medium of instruction')}</p>
-        <DropDown
-       currentValue={currentlySelectedLang}
-       placeholder={t("Select").toString()}
-        onValueChange={onLangChange}
+            width="26vw"
+          />
+        </div>
+        <div id="drop-down-Medium-of-instruction">
+          <p id="drop-down-head">{t('Medium of instruction')}</p>
+          <DropDown
+            currentValue={currentlySelectedLang}
+            placeholder={t("Select Language").toString()}
+            onValueChange={onLangChange}
         optionList={languageOptions??[]}
-        width="26vw"
-      />
+            width="26vw"
+          />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default GradeBoardAndLangDropdown;
