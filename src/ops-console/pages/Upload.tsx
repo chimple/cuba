d5f6ba49d3d1157599f28a46c30a6c0f88dc3c33
guddu-sutara <guddukumar.prasad@sutara.org>
2025-03-19 09:46:10 +0530

    updated

--- src/ops-console/pages/Upload.tsx
+++ src/ops-console/pages/Upload.tsx
@@ -1,8 +1,27 @@
+import { useHistory } from "react-router-dom";
+import Header from "../../chimple-private/components/homePage/Header";
+import { PAGES } from "../../common/constants";
 import FileUpload from "../components/FileUpload";
 
 const Upload = () => {
+
+  const history = useHistory();
+  
+  const onBackButtonClick = () => {
+      history.replace(PAGES.HOME_PAGE, {
+        tabValue: 0,
+      });
+    };
+    
   return (
-    <div>
+    <div className="main-page">
+      <div className="fixed-header">
+        <Header
+          isBackButton={true}
+          onBackButtonClick={onBackButtonClick}
+          onButtonClick={undefined}
+        />
+      </div>
       <FileUpload />
     </div>
   );
