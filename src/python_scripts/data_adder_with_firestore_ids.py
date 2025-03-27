2960b904a100ac853bfe08a8255b6696824af49c
narayanangourav-sutara <narayana.gourav@sutara.org>
2025-03-27 12:12:24 +0530

    Script for course migration from firebase to supabase

---
+++ src/python_scripts/data_adder_with_firestore_ids.py
@@ -0,0 +1,164 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+from supabase import create_client, Client
+import unicodedata
+from datetime import datetime
+import time
+import json
+
+# Helper Functions
+def get_subject_id(doc_data, supabase):
+    def normalize(text):
+        return unicodedata.normalize('NFC', text.strip()) if text else ''
+    title = normalize(get_the_doc(doc_data.get("subject")).get("title"))
+    response = supabase.table("subject").select("id", "name").execute()
+    for row in response.data:
+        if normalize(row["name"]) == title:
+            return row["id"]
+    return None
+
+def get_grade_id(doc_data, supabase):
+    def normalize(text):
+        return unicodedata.normalize('NFC', text.strip()) if text else ''
+    title = normalize(get_the_doc(doc_data.get("grade")).get("title"))
+    response = supabase.table("grade").select("id", "name").execute()
+    for row in response.data:
+        if normalize(row["name"]) == title:
+            return row["id"]
+    return None
+
+def get_curriculum_id(doc_data, supabase):
+    def normalize(text):
+        return unicodedata.normalize('NFC', text.strip()) if text else ''
+    title = normalize(get_the_doc(doc_data.get("curriculum")).get("title"))
+    response = supabase.table("curriculum").select("id", "name").execute()
+    for row in response.data:
+        if normalize(row["name"]) == title:
+            return row["id"]
+    return None
+
+def get_language_id(doc_data, supabase):
+    def normalize(text):
+        return unicodedata.normalize('NFC', text.strip()) if text else ''
+    title = normalize(get_the_doc(doc_data.get("language")).get("title"))
+    response = supabase.table("language").select("id", "name").execute()
+    for row in response.data:
+        if normalize(row["name"]) == title:
+            return row["id"]
+    return None
+
+def get_the_doc(ref):
+    if isinstance(ref, firestore.DocumentReference):
+        doc = ref.get()
+        if doc.exists:
+            val = doc.to_dict()
+            return val
+        
+def normalize_datetimes(data):
+    for key, value in data.items():
+        if isinstance(value, datetime):
+            data[key] = value.isoformat()
+    return data
+
+def fetch_documents_by_id_one_by_one(doc_ids):
+    for doc_id in doc_ids:
+        doc_ref = docs.document(doc_id)
+        doc = doc_ref.get()
+        if doc.exists:
+            yield doc.to_dict()
+        else:
+            print(f"Document with ID {doc_id} not found.")
+        print("**Sleep Started**")
+        time.sleep(180)
+        print("**Started Document**")
+
+def get_the_doc(ref):
+    if isinstance(ref, firestore.DocumentReference):
+        doc = ref.get()
+        if doc.exists:
+            val = doc.to_dict()
+            return val
+
+# Firebase setup
+FIREBASE_CREDENTIALS_PATH = "serviceAccountKey.json"  # Replace with your Firebase credentials
+firebase_admin.initialize_app(credentials.Certificate(FIREBASE_CREDENTIALS_PATH))
+db = firestore.client()
+if db:
+    print("**Firebase connected successfully.**")
+
+# Supabase setup
+SUPABASE_URL, SUPABASE_KEY = (lambda c: (c['S_URL'], c['S_KEY']))(json.load(open('serviceAccountKeySupabase.json')))
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+if supabase:
+    print("**Supabase connected successfully.**")
+
+print("**--------------------------------**")
+
+# List of document IDs you want to fetch from firestore
+document_ids = ['*', '*', '*']
+
+# Firebase table
+collection_name = "Course"
+docs = db.collection(collection_name)
+        
+# Usage
+for doc_data in fetch_documents_by_id_one_by_one(document_ids):
+    insert_data = {
+        "name": doc_data['title'],
+        "image": doc_data.get("thumbnail","null"),
+        "description": "",
+        "sort_index": doc_data.get('sortIndex', 256),
+        "subject_id": get_subject_id(doc_data, supabase),
+        "grade_id": get_grade_id(doc_data, supabase),
+        "curriculum_id": get_curriculum_id(doc_data, supabase),
+        "is_deleted": False,
+        "color": doc_data.get("color","null"),
+        "code": doc_data['courseCode']
+    }
+    response = supabase.table("course").insert(insert_data).execute()
+    if response:
+        print(f"✅ Inserted in course: {response.data[0]['id']}")
+        course_id = response.data[0]['id']
+        chapter = doc_data['chapters']
+        for idx, ch_data in enumerate(chapter):
+            chapter_resp = {
+                "name": ch_data['title'],
+                "image": ch_data.get("thumbnail","null"),
+                "course_id": course_id,
+                "sort_index": idx,
+                "sub_topics": "",
+                "is_deleted": False
+            }
+            cr = supabase.table("chapter").insert(chapter_resp).execute()
+            print(f"✅ Inserted in chapter: {cr.data[0]['id']}")
+            chapter_id = cr.data[0]['id']
+            for idx, lesson_doc in enumerate(ch_data['lessons']):
+                ln_data = lesson_doc.get().to_dict()
+                lesson_resp = {
+                    "name": ln_data['title'],
+                    "image": ln_data.get("thumbnail","null"),
+                    "outcome": ln_data.get("outcome","null"),
+                    "plugin_type": ln_data['pluginType'],
+                    "status": ln_data['status'],
+                    "cocos_subject_code": ln_data['cocosSubjectCode'],
+                    "cocos_chapter_code": ln_data['cocosChapterCode'],
+                    "subject_id": get_subject_id(ln_data, supabase),
+                    "target_age_from": ln_data['targetAgeFrom'],
+                    "target_age_to": ln_data['targetAgeTo'],
+                    "language_id": get_language_id(ln_data, supabase),
+                    "is_deleted": False,
+                    "cocos_lesson_id": ln_data['id'],
+                    "color": "",
+                }
+                lesson_resp = normalize_datetimes(lesson_resp)
+                lr = supabase.table("lesson").insert(lesson_resp).execute()
+                print(f"✅ Inserted in lesson: {lr.data[0]['id']}")
+                lesson_id = lr.data[0]['id']
+                cl = supabase.table("chapter_lesson").insert({ 
+                    "lesson_id": lesson_id,
+                    "chapter_id": chapter_id,
+                    "sort_index": idx,
+                    "is_deleted": False,
+                }).execute()
+                print(f"✅ Inserted in chapter_lesson: {cl.data[0]['id']}")
+    
---
+++ src/python_scripts/py/data_adder_subject.py
@@ -0,0 +1,51 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+from supabase import create_client, Client
+import json
+
+firebase_list = []
+supabase_list = []
+
+# Firebase setup
+FIREBASE_CREDENTIALS_PATH = "serviceAccountKey.json"  # Replace with your Firebase credentials
+firebase_admin.initialize_app(credentials.Certificate(FIREBASE_CREDENTIALS_PATH))
+db = firestore.client()
+if db:
+    print("**Firebase connected successfully.**")
+
+# Supabase setup
+SUPABASE_URL, SUPABASE_KEY = (lambda c: (c['S_URL'], c['S_KEY']))(json.load(open('serviceAccountKeySupabase.json')))
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+if supabase:
+    print("**Supabase connected successfully.**")
+
+print("**--------------------------------**")
+
+# Firebase table
+collection_name = "Subject"
+firebase_docs = db.collection(collection_name).stream()
+data_list = []
+for doc in firebase_docs:
+    doc_data = doc.to_dict()
+    doc_data["id"] = doc.id 
+    data_list.append(doc_data)
+if data_list:
+    for data in data_list:
+        firebase_list.append(data['title'])
+
+# Supabase table
+print()
+response = supabase.table("subject").select("*").execute()
+for i in response.data:
+    supabase_list.append(i['name'])
+
+matches = [word for word in firebase_list if any(word in item for item in supabase_list)]
+non_matches = [word for word in firebase_list if word not in matches]
+print("Existed vales from tables:",matches)
+print("Non-Existed vales from tables:",non_matches)
+
+#Inserting into supabase table
+if len(non_matches) > 0:
+    for name in non_matches:
+        response = supabase.table("subject").insert({"name": name}).execute()
+        print(f"Inserted: {name}, Response: {response}")
