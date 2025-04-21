0c0006a968ec71befebea7f87e312fee84865711
guddu-sutara <guddukumar.prasad@sutara.org>
2025-04-21 09:45:24 +0530

    Update CheckWhatsapp.ts

--- src/ops-console/OpsUtility/CheckWhatsapp.ts
+++ src/ops-console/OpsUtility/CheckWhatsapp.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { RoleType } from "../../interface/modelInterfaces";
+import { USER_ROLE } from "../../common/constants";
 
 const RAPID_API_KEY = process.env.REACT_APP_RAPIDAPI_KEY;
 
@@ -24,22 +26,33 @@ async function validateWhatsappNumbers(phoneNumbers: string[]): Promise<any> {
   }
 }
 
-export const checkWhatsappNumbers = async (numbers: string[]) => {
-  const results: { number: string; status: string; onWhatsapp: boolean }[] = [];
+export const checkWhatsappNumbers = async (numbers: string[]) => {            //number shuould be E.164 ("919876543210") format
+  const currentUserRole = JSON.parse(localStorage.getItem(USER_ROLE)!);
 
-  for (let i = 0; i < numbers.length; i += 10) {
-    const batch = numbers.slice(i, i + 10);
-    const data = await validateWhatsappNumbers(batch);
+  if (
+    currentUserRole === RoleType.PROGRAM_MANAGER ||
+    currentUserRole === RoleType.FIELD_COORDINATOR ||
+    currentUserRole === RoleType.OPERATIONAL_DIRECTOR
+  ) {
+    const results: { number: string; status: string; onWhatsapp: boolean }[] =
+      [];
 
-    for (const item of data) {
-      results.push({
-        number: item.phone_number,
-        status: item.status,
-        onWhatsapp: (item.status == "valid") ? true : false,
-      });
+    for (let i = 0; i < numbers.length; i += 10) {
+      const batch = numbers.slice(i, i + 10);
+      const data = await validateWhatsappNumbers(batch);
+
+      for (const item of data) {
+        results.push({
+          number: item.phone_number,
+          status: item.status,
+          onWhatsapp: item.status == "valid" ? true : false,
+        });
+      }
+      await new Promise((res) => setTimeout(res, 1000));
     }
-    await new Promise((res) => setTimeout(res, 1000));
+    return results;
+  } else {
+    console.error("You don't have permission to validate WhatsApp numbers.");
+    return [];
   }
-
-  return results;
 };
