60172e997bbedfefd778547519bc2c3fd01d02e0
Manish Joshi <manish.joshi@sutara.org>
2025-04-28 10:07:39 +0530

    style: fixed chapterlessonbox styling

--- src/components/LearningPathway.tsx
+++ src/components/LearningPathway.tsx
@@ -5,27 +5,10 @@ const LearningPathway: React.FC = () => {
   return (
     <div>
       <PathwayStructure />
-      <ChapterLessonBox 
+      <ChapterLessonBox
         text="English: a-d"
         containerStyle={{
-          position: 'fixed',
-          bottom: '2vh',
-          left: '50%',
-          transform: 'translateX(-50%)',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundImage: 'url(/pathwayAssets/chapterLessonBox.svg)',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-          backgroundSize: '100% 100%',
-          width: '27vw'
-        }}
-        textStyle={{
-          margin: 0,
-          color: 'var(--text-color)',
-          textAlign: 'center',
-          fontSize: 'var(--text-sizeL)'
+          width: "27vw",
         }}
       />
     </div>
--- src/components/learningPathway/chapterLessonBox.tsx
+++ src/components/learningPathway/chapterLessonBox.tsx
@@ -3,17 +3,35 @@ import React from "react";
 interface ChapterLessonBoxProps {
   text: string;
   containerStyle?: React.CSSProperties;
-  textStyle?: React.CSSProperties;
 }
+
+
 
 const ChapterLessonBox: React.FC<ChapterLessonBoxProps> = ({
   text,
   containerStyle,
-  textStyle,
 }) => {
   return (
-    <div style={containerStyle}>
-      <div style={textStyle}>{text}</div>
+    <div style={{
+      ...containerStyle,
+      position: 'fixed',
+      bottom: '2vh',
+      left: '50%',
+      transform: 'translateX(-50%)',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundImage: "url('/pathwayAssets/chapterLessonBox.svg')",
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundSize: '100% 100%'
+    }}>
+      <div style={{
+        margin: 0,
+        color: 'var(--text-color)',
+        textAlign: 'center',
+        fontSize: 'var(--text-sizeL)'
+      }}>{text}</div>
     </div>
   );
 };
