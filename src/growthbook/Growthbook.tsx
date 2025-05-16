c7828051bc48bd964461845434500831551ade8f
sangram-mkj-sutara <sangram.mukherjee@sutara.org>
2025-05-16 15:30:41 +0530

    added growthbook page

---
+++ src/growthbook/Growthbook.tsx
@@ -0,0 +1,72 @@
+import React, { createContext, useState, useContext, ReactNode, useEffect } from 'react';
+import { useGrowthBook } from '@growthbook/growthbook-react';
+type GbContextType = {
+  gbUpdated: boolean;
+  setGbUpdated: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const GbContext = createContext<GbContextType | undefined>(undefined);
+
+export const updateLocalAttributes = (data) => {
+  let existingData = localStorage.getItem('growthbookAttributes');
+  let parsedData = existingData ? JSON.parse(existingData) : {};
+  const updatedData = {
+    ...parsedData,
+    ...data
+  }
+  localStorage.setItem('growthbookAttributes', JSON.stringify(updatedData));
+}
+
+export const GbProvider = ({ children }: { children: ReactNode }) => {
+  const growthbook = useGrowthBook();
+  const [gbUpdated, setGbUpdated] = useState(false);
+
+  useEffect(() => {
+    if(gbUpdated){
+      const storedAttributes = localStorage.getItem('growthbookAttributes');
+      if (storedAttributes) {
+        const attributes = JSON.parse(storedAttributes);
+        setGrowthbookAttributes(attributes);
+        setGbUpdated(false)
+        console.log("console from growthbook: ", attributes)
+      }
+    }
+  }, [gbUpdated])
+
+  const setGrowthbookAttributes = (student: any) => {
+    const {studentDetails, schools, classes, liveQuizCount, assignmentCount, countOfPendingIds, last_assignment_played_at, total_assignments_played} = student;
+
+    growthbook.setAttributes({
+      id: studentDetails.id,
+      age: studentDetails.age,
+      curriculum_id: studentDetails.curriculum_id,
+      grade_id: studentDetails.grade_id,
+      gender: studentDetails.gender,
+      parent_id: studentDetails.parent_id,
+      subject_id: studentDetails.subject_id,
+      school_ids: schools,
+      class_ids: classes,
+      language: localStorage.getItem("language") || "en",
+      stars: studentDetails.stars,
+      pending_live_quiz: liveQuizCount,
+      pending_assignments: assignmentCount,
+      ...countOfPendingIds,
+      last_assignment_played_at: last_assignment_played_at,
+      total_assignments_played: total_assignments_played,
+    });
+  };
+
+  return (
+    <GbContext.Provider value={{ gbUpdated, setGbUpdated }}>
+      {children}
+    </GbContext.Provider>
+  );
+};
+
+export const useGbContext = () => {
+  const context = useContext(GbContext);
+  if (!context) {
+    throw new Error('useGbContext must be used within a GbProvider');
+  }
+  return context;
+};
