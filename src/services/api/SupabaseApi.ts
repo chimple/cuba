192e13103c995c0fa8ca95a2cb41a21a397cb69d
Vinay Reddy <62736791+vinaypanduga@users.noreply.github.com>
2025-05-16 10:49:09 +0530

    Update SupabaseApi.ts

--- src/services/api/SupabaseApi.ts
+++ src/services/api/SupabaseApi.ts
@@ -391,6 +391,107 @@ export class SupabaseApi implements ServiceApi {
     }
     return data;
   }
+  async validateSchoolUdiseCode(
+    schoolId: string
+  ): Promise<{ status: string; errors?: string[] }> {
+    if (!this.supabase) {
+      return {
+        status: "error",
+        errors: ["Supabase client is not initialized"],
+      };
+    }
+
+    try {
+      const { data, error } = await this.supabase.rpc(
+        "validate_school_udise_code",
+        {
+          input_school_udise_code: schoolId,
+        }
+      );
+      if (data?.status === "error" && (data as any).message) {
+        return {
+          status: "error",
+          errors: [(data as any).message],
+        };
+      }
+      return data as { status: string; errors?: string[] };
+    } catch (error) {
+      return {
+        status: "error",
+        errors: [String(error)],
+      };
+    }
+  }
+  async validateClassNameWithSchoolID(
+    schoolId: string,
+    className: string,
+  ): Promise<{ status: string; errors?: string[] }> {
+    if (!this.supabase) {
+      return {
+        status: "error",
+        errors: ["Supabase client is not initialized"],
+      };
+    }
+
+    try {
+      const { data, error } = await this.supabase.rpc(
+        "check_class_exists_by_name_and_school",
+        {
+          class_name: className,
+          input_school_udise_code: schoolId,
+        }
+      );
+      if (data?.status === "error" && (data as any).message) {
+        return {
+          status: "error",
+          errors: [(data as any).message],
+        };
+      }
+      return data as { status: string; errors?: string[] };
+    } catch (error) {
+      return {
+        status: "error",
+        errors: [String(error)],
+      };
+    }
+  }
+  async validateStudentInClassWithoutPhone(
+    studentName: string,
+    className: string,
+    schoolId: string
+  ): Promise<{ status: string; errors?: string[] }> {
+    if (!this.supabase) {
+      return {
+        status: "error",
+        errors: ["Supabase client is not initialized"],
+      };
+    }
+
+    try {
+      const { data, error } = await this.supabase.rpc(
+        "check_student_duplicate_in_class_without_phone_number",
+        {
+          student_name: studentName,
+          class_name: className,
+          input_school_udise_code: schoolId,
+        }
+      );
+
+      if (data?.status === "error" && (data as any).message) {
+        return {
+          status: "error",
+          errors: [(data as any).message],
+        };
+      }
+
+      return data as { status: string; errors?: string[] };
+    } catch (error) {
+      return {
+        status: "error",
+        errors: [String(error)],
+      };
+    }
+  }
 
   async mutate(
     mutateType: MUTATE_TYPES,
