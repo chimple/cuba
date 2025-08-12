1bb4b8b658f893e5a7e06e82b7bc54805a442700
Vinay Reddy <vpanduga@gmail.com>
2025-08-12 16:17:39 +0530

    Revert "SQL CHANGES"
    This reverts commit 021eb2e3c76b5fe1d0bc6c52f8cf454cc7df33d6.

--- src/services/api/deleted.sql
+++
@@ -1,5 +0,0 @@
-create index IF not exists idx_special_users_role_user_id on public.special_users using btree (role, user_id) TABLESPACE pg_default;
-create index IF not exists idx_special_users_user_role on public.special_users using btree (user_id, role) TABLESPACE pg_default;
-create index IF not exists idx_special_users_active_user_role on public.special_users using btree (user_id, role) TABLESPACE pg_default
-where
-  (is_deleted = false);
--- src/services/api/newindexes.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX idx_program_user_user ON public.program_user("user");
-CREATE INDEX idx_program_user_program_id ON public.program_user(program_id);
-CREATE INDEX idx_program_user_role ON public.program_user(role);
-CREATE INDEX idx_program_user_is_deleted ON public.program_user(is_deleted);
