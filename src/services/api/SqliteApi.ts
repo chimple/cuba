ffa4a663d6de3741e03decbfcf320855918cd05f
guddu-kumar <guddukumar.prasad@sutara.org>
2025-09-24 12:14:24 +0530

    Update SqliteApi.ts

--- src/services/api/SqliteApi.ts
+++ src/services/api/SqliteApi.ts
@@ -4787,6 +4787,7 @@ order by
       `
     INSERT INTO school_user (id, school_id, user_id, role, created_at, updated_at, is_deleted)
     VALUES (?, ?, ?, ?, ?, ?, ?)
+    ON CONFLICT (school_id, user_id, role) DO NOTHING;
     `,
       [
         schoolUser.id,
