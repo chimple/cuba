fa90ba07bc0e1ea73b659571b926337f10cde469
Rahul-Chimple <rahul.lanka@sutara.org>
2025-10-08 16:43:30 +0530

    commented search school page code navigation

--- src/teachers-module/pages/DisplaySchools.tsx
+++ src/teachers-module/pages/DisplaySchools.tsx
@@ -82,10 +82,10 @@ const DisplaySchools: FC = () => {
       page: pageNo,
       page_size: PAGE_SIZE,
     });
-    if (pageNo === 1 && result.length === 0) {
-      history.replace(PAGES.SEARCH_SCHOOL);
-      return; 
-    }
+    // if (pageNo === 1 && result.length === 0) {
+    //   history.replace(PAGES.SEARCH_SCHOOL);
+    //   return; 
+    // }
     if (result.length < PAGE_SIZE) setHasMore(false);
     setSchoolList((prev) => (pageNo === 1 ? result : [...prev, ...result]));
     setLoading(false);
