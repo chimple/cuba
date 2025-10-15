10f8a9e05fc0ae3257366be90fc7d546920f5bac
ALOK-TV <alok@sutara.org>
2025-10-15 19:58:43 +0530

    fixed translations issues also

--- src/components/displaySubjects/SelectChapter.tsx
+++ src/components/displaySubjects/SelectChapter.tsx
@@ -3,7 +3,7 @@ import "./SelectChapter.css";
 import SelectIconImage from "./SelectIconImage";
 import DownloadLesson from "../DownloadChapterAndLesson";
 import { t } from "i18next";
-import { TableTypes } from "../../common/constants";
+import { COURSES, TableTypes } from "../../common/constants";
 
 const SelectChapter: FC<{
   chapters: TableTypes<"chapter">[];
@@ -54,7 +54,11 @@ const SelectChapter: FC<{
                     imageHeight={"auto"}
                   />
                 </div>
-                <div>{t(chapter.name ?? "")}</div>
+                <div>
+                  {course?.code === COURSES.ENGLISH
+                    ? chapter?.name
+                    : t(chapter?.name ?? "")}
+                </div>
                 <div className="chapter-download">
                   <DownloadLesson chapter={chapter} />
                 </div>
