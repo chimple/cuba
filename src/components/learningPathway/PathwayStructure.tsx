6a3c2bad8fda1fa5cd20cab8fbf238be8eb2f6a1
niteshmandal0 <108911562+niteshmandall@users.noreply.github.com>
2025-10-15 15:39:53 +0530

    refactor: getting current student

--- src/components/learningPathway/PathwayStructure.tsx
+++ src/components/learningPathway/PathwayStructure.tsx
@@ -197,8 +197,9 @@ const PathwayStructure: React.FC = () => {
 
       try {
         const startTime = performance.now();
-
-        const currentStudent = Util.getCurrentStudent();
+        const currentStudent = Util.isRespectMode 
+          ? Util.getCurrentStudent() 
+          : updatedStudent || await Util.getCurrentStudent();
         const learningPath = currentStudent?.learning_path
           ? JSON.parse(currentStudent.learning_path)
           : null;
