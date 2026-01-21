f5ed35cfefefc7a0285ed361a7970eacdea26436
Rahul-Chimple <rahul.lanka@sutara.org>
2026-01-21 10:31:21 +0530

    update

--- src/components/profileDetails/ProfileDetails.tsx
+++ src/components/profileDetails/ProfileDetails.tsx
@@ -241,7 +241,7 @@ const ProfileDetails = () => {
           undefined,
           undefined,
           undefined,
-          languageId || languages[1].id
+          languageId || languages.find((lang) => lang.code === "en")?.id
         );
         Util.logEvent(EVENTS.PROFILE_CREATED, {
           user_id: user?.id,
@@ -254,9 +254,8 @@ const ProfileDetails = () => {
           page_path: window.location.pathname,
           action_type: ACTION_TYPES.PROFILE_CREATED,
         });
-        const langIndex = languages?.findIndex(
-          (lang) => lang.id === languages[1].id
-        );
+        
+        const langIndex = languages?.findIndex((lang) => lang.id === languageId);
         await Util.setCurrentStudent(
           student,
           langIndex && languages && languages[langIndex]?.code
