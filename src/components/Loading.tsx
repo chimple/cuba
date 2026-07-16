ffc782c352cd0eeaba210cadf8c67353e46d7ff3
aneeshgagan <aneesh.gagan@sutara.org>
2026-07-16 15:44:41 +0530

    b

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import './Loading.css';
-import { t } from 'i18next';
 
 interface LoadingProps {
   isLoading: boolean;
   msg?: string;
 }
 
-const Loading: React.FC<LoadingProps> = ({ isLoading, msg = '' }) => {
+const Loading: React.FC<LoadingProps> = ({ isLoading }) => {
   if (!isLoading) return null;
 
   return (
     <div className="loading-overlay">
-      <div className="loading-container">
-        <img src="assets/loading.gif" alt="loading" className="loading-img" />
-
-        {msg && <p className="loading-msg">{t(msg)}</p>}
-      </div>
+      <img
+        src="assets/Atking-loading.png"
+        alt="Loading"
+        className="loading-img"
+      />
     </div>
   );
 };
